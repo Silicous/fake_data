bf7883f62303256f8fa0c81b88f400c92552e870
Sergio <serhro@st.amu.edu.pl>
2021-09-27 13:57:21 +0300

    fix procfile

--- fake_data/settings.py
+++ fake_data/settings.py
@@ -149,8 +149,8 @@ STATICFILES_FINDERS = (
 
 # STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-BROKER_URL = os.environ.get('BROKER_URL')
-CELERY_RESULT_BACKEND = BROKER_URL
+CELERY_BROKER_URL = 'redis://localhost:6379'
+CELERY_RESULT_BACKEND = 'redis://localhost:6379'
 CELERY_ACCEPT_CONTENT = ['application/json']
 CELERY_RESULT_SERIALIZER = 'json'
 CELERY_TASK_SERIALIZER = 'json'
